16790f9612a1bfe195f6329b7a3b031fd7f938eb
Yashashvi-Yadv <timtimabhi@gmail.com>
2026-09-13 22:17:18 +0530

    update on backend

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
