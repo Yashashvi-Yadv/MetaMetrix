8028dae1b1d01a6e897418e3f221afd0f3f376fe
Yashashvi-Yadv <timtimabhi@gmail.com>
2026-09-14 18:27:38 +0530

    s

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: "all", // Development server ke liye saare hosts allow karega
+  },
+  preview: {
+    allowedHosts: "all", // Production/Preview deployment ke liye host block hatayega
+  },
 });
