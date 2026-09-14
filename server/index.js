d869261ee0bdf75dee8cb6eb9dab00c00bd31799
Yashashvi-Yadv <timtimabhi@gmail.com>
2026-09-14 17:59:25 +0530

    d

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
