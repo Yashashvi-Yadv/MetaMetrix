f6a7a13590aced6cc19320dbf2e6c3b16e29371f
Yashashvi-Yadv <timtimabhi@gmail.com>
2026-09-14 18:37:33 +0530

    g

--- client/src/components/AuthContext.jsx
+++ client/src/components/AuthContext.jsx
@@ -18,6 +18,7 @@ export const AuthProvider = ({ children }) => {
   });
 
   const login = (token, userData) => {
+    Cookies.set("token", token);
     localStorage.setItem("user", JSON.stringify(userData));
     setUser(userData);
     setIsAuthenticated(true);
