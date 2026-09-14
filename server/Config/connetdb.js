2978627507f4f5855a766c74afbe1b2d763baf37
Yashashvi-Yadv <timtimabhi@gmail.com>
2026-09-14 18:03:37 +0530

    ds

--- server/Config/connetdb.js
+++ server/Config/connetdb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected Successfully!");
+    console.log("✅ MongoDB Connected Successfully!"); //
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err.message);
     process.exit(1);
